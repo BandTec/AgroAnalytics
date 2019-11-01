d15f2a1869ef92760211260094a5bda8fb7e67f3
0BlackSmoke0 <bertucci.henrique@gmail.com>
2019-11-01 18:51:08 -0300

    Controller de Usuario Ok (POST GET PUT DELITE )

--- simplexagro/simplexagro/src/main/java/br/com/agroanalytics/simplexagro/service/UsuarioService.java
+++ simplexagro/simplexagro/src/main/java/br/com/agroanalytics/simplexagro/service/UsuarioService.java
@@ -1,13 +1,43 @@
 package br.com.agroanalytics.simplexagro.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.agroanalytics.simplexagro.domain.Usuario;
 import br.com.agroanalytics.simplexagro.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
 
 	@Autowired
-	UsuarioRepository usuarioRepository;
+	private UsuarioRepository usuarioRepository;
+
+	public List<Usuario> buscarTodosUsuarios() {
+
+		List<Usuario> usuarios = usuarioRepository.findAll();
+
+		return usuarios;
+
+	}
+
+	public Usuario adicionarUsuario(Usuario usuario) {
+
+		return usuarioRepository.save(usuario);
+
+	}
+
+	public void excluirUsuario(Long idUsuario) {
+		
+
+		this.usuarioRepository.deleteById(idUsuario);;
+
+	}
+
+	public Usuario editarUsuario(Usuario usuario) {
+
+		return usuarioRepository.save(usuario);
+	}
+
 }
