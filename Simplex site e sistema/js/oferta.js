7fef3a1bbfee5da73ef6cfda042fa462060e9a40
Cleri Ariane dos Santos Rocha <cleri.ariane@totvs.com.br>
2019-12-16 18:02:04 -0300

    formulario de ofertas e link de leilão

---
+++ Simplex site e sistema/js/oferta.js
@@ -0,0 +1,37 @@
+var enviarOferta = document.querySelector("#realizar_oferta");
+
+enviarOferta.addEventListener("click", function (){
+
+    var form = document.querySelector("#oferta");
+
+    var oferta = {
+
+        idColheita: form.colheita.value,
+        valorOferta: form.oferta.value,
+        quantCaixasCompradas: form.caixas.value
+        
+    }
+
+    var xhr =  new XMLHttpRequest();
+
+    xhr.open("POST","http://localhost:8080/ofertas");
+
+    xhr.setRequestHeader("Content-type", "application/json");
+
+    xhr.send(JSON.stringify(oferta));
+
+    xhr.addEventListener("load", function(){
+
+        var resposta = xhr.responseText;
+
+        alert(resposta);
+
+        var oferta = JSON.parse(resposta);
+
+        alert(oferta);
+
+    }
+
+
+)
+});
